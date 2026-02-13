adaf8d7c487058d8f08cec1dbf12418997eecabb
Pedro <pedro@example.com>
2026-02-12 22:10:46 -0300

    Remove legacy firmware files - focus on ESP32-S3 only

--- release/s3_compile_essentials/firmware/s3/components/engine_control/include/ignition_timing.h
+++
@@ -1,9 +0,0 @@
-#ifndef IGNITION_TIMING_H
-#define IGNITION_TIMING_H
-
-#include <stdint.h>
-
-void ignition_init(void);
-void ignition_apply_timing(uint16_t advance_deg10, uint16_t rpm);
-
-#endif // IGNITION_TIMING_H
